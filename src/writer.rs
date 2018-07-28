afb92ad2857f6b35fe6cea2489bf3ca691b0ef55
Alexander Reiff <alexander.reiff@me.com>
2018-07-28 15:08:27 -0700

    simplify writer

--- src/writer.rs
+++ src/writer.rs
@@ -17,14 +17,8 @@ impl Writer {
     pub fn write(&self, contents: &str) -> Result<(), Box<Error>> {
         let path = Path::new(&self.outfile);
 
-        let mut file = match File::create(&path) {
-            Ok(file) => file,
-            Err(err) => return Err(From::from(err)),
-        };
-
-        match file.write_all(contents.as_bytes()) {
-            Ok(_) => Ok(()),
-            Err(err) => Err(From::from(err)),
-        }
+        let mut file = File::create(&path)?;
+        file.write_all(contents.as_bytes())?;
+        Ok(())
     }
 }
